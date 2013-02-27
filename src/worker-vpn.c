724a7ac3b5f5eb3e9468377ce9919379294f72d7
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-02-27 20:31:34 +0100

    more verbose in client methods

--- src/worker-vpn.c
+++ src/worker-vpn.c
@@ -452,6 +452,7 @@ restart:
 	} while(ws->req.headers_complete == 0);
 
 	if (parser.method == HTTP_GET) {
+		oclog(ws, LOG_DEBUG, "HTTP GET %s", ws->req.url); 
 		fn = get_url_handler(ws->req.url);
 		if (fn == NULL) {
 			oclog(ws, LOG_INFO, "unexpected URL %s", ws->req.url); 
@@ -465,6 +466,7 @@ restart:
 
 	} else if (parser.method == HTTP_POST) {
 		/* continue reading */
+		oclog(ws, LOG_DEBUG, "HTTP POST %s", ws->req.url); 
 		while(ws->req.message_complete == 0) {
 			nrecvd = tls_recv(session, buf, sizeof(buf));
 			GNUTLS_FATAL_ERR(nrecvd);
@@ -488,12 +490,13 @@ restart:
 			goto restart;
 
 	} else if (parser.method == HTTP_CONNECT) {
+		oclog(ws, LOG_DEBUG, "HTTP CONNECT %s", ws->req.url); 
 		ret = connect_handler(ws);
 		if (ret == 0 && (parser.http_major != 1 || parser.http_minor != 0))
 			goto restart;
 
 	} else {
-		oclog(ws, LOG_INFO, "unexpected method %s", http_method_str(parser.method)); 
+		oclog(ws, LOG_INFO, "unexpected HTTP method %s", http_method_str(parser.method)); 
 		tls_puts(session, "HTTP/1.1 404 Nah, go away\r\n\r\n");
 	}
 

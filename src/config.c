b2e8137ddefddeaf13da1fa02031eebe7b8e762a
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-03-06 23:43:51 +0100

    rate-limit-ms is no longer mandatory to set

--- src/config.c
+++ src/config.c
@@ -142,7 +142,7 @@ unsigned j;
 	READ_NUMERIC("udp-port", config->udp_port, 0);
 	READ_NUMERIC("keepalive", config->keepalive, 0);
 	READ_NUMERIC("dpd", config->dpd, 0);
-	READ_NUMERIC("rate-limit-ms", config->rate_limit_ms, 10);
+	READ_NUMERIC("rate-limit-ms", config->rate_limit_ms, 0);
 
 	READ_STRING("ocsp-response", config->cert, 0);
 	READ_STRING("server-cert", config->cert, 1);

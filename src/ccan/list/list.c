08efccdfb19e48c5c9a8e30b2c57d92dc5b010e7
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-03-15 19:45:14 +0100

    include config.h in all files

--- src/ccan/list/list.c
+++ src/ccan/list/list.c
@@ -1,4 +1,5 @@
 /* Licensed under BSD-MIT - see LICENSE file for details */
+#include <config.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
--- src/http-parser/http_parser.c
+++ src/http-parser/http_parser.c
@@ -21,6 +21,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
+#include <config.h>
 #include "http_parser.h"
 #include <assert.h>
 #include <stddef.h>

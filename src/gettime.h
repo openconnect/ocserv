b3cdd31dca0afa47386e0bf7f160ee8846752bf3
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-05-20 11:03:50 +0200

    Added missing file

---
+++ src/gettime.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2010-2012 Free Software Foundation, Inc.
+ *
+ * Author: Nikos Mavrogiannopoulos
+ *
+ * This file is part of ocserv.
+ *
+ * The GnuTLS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+#ifndef GETTIME_H
+#define GETTIME_H
+
+#include <config.h>
+#include <time.h>
+#include <sys/time.h>
+
+/* emulate gnulib's gettime using gettimeofday to avoid linking to
+ * librt */
+inline static void
+gettime (struct timespec *t)
+{
+#if defined(HAVE_CLOCK_GETTIME) && defined(CLOCK_REALTIME)
+  clock_gettime (CLOCK_REALTIME, t);
+#else
+struct timeval tv;
+  gettimeofday (&tv, NULL);
+  t->tv_sec = tv.tv_sec;
+  t->tv_nsec = tv.tv_usec * 1000;
+#endif
+}
+
+#endif

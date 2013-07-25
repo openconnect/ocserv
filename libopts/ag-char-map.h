2cdfafcd5bd21a3efca286281049655b7f0cbca6
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-07-25 23:11:49 +0200

    Revert "Ignore non-ascii characters in configuration file."
    This reverts commit f7a938e5d7fd07144062ea64a6ab028cf43bb3e6.

--- libopts/ag-char-map.h
+++ libopts/ag-char-map.h
@@ -489,8 +489,7 @@ spn_ag_char_map_chars(char const * p, unsigned int mask_ix)
     unsigned char const * v = ag_char_map_spanners[mask_ix];
     if (v == NULL)
         v = calc_ag_char_map_spanners(mask_ix);
-
-    while (!isascii(*p) || v[(unsigned)*p])  p++;
+    while (v[(unsigned)*p])  p++;
     return (char *)(uintptr_t)p;
 }
 

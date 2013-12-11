dd162faf3629cbe844a2c35c71dc3a37e6547892
Nikos Mavrogiannopoulos <nmav@redhat.com>
2013-12-11 08:50:30 +0100

    reorder

--- src/worker-tun.c
+++ src/worker-tun.c
@@ -32,11 +32,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <limits.h>
-#include <net/if.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <net/if.h>
 #include <arpa/inet.h>
 #include <signal.h>
 #include <time.h>

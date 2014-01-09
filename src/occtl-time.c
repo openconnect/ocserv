b4c4244ef611e2d6dc3f13e8d870d559e0493ddc
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-01-09 15:34:33 +0100

    doc update

--- src/occtl-time.c
+++ src/occtl-time.c
@@ -7,6 +7,7 @@
  * Michael K. Johnson.
  *
  * Changes by Albert Cahalan, 2002.
+ * Modified for occtl by Nikos Mavrogiannopoulos, 2013.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -18,9 +19,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>

8c82e8c96ca3fe2a13c1382e5d898fa1e0e544ec
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-05-27 14:35:30 +0200

    Overwrite TLS session data prior to release.

--- src/main-resume.c
+++ src/main-resume.c
@@ -183,6 +183,7 @@ void expire_tls_sessions(main_server_st * s)
 			cache->session_id_size = 0;
 
 			htable_delval(&s->tls_db->ht, &iter);
+			safe_memset(cache->session_data, 0, cache->session_data_size);
 			talloc_free(cache);
 			s->tls_db->entries--;
 		}

70eea332ffe24d325c2e535cafb8b9e3bf576b62
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-08-25 21:52:04 +0200

    check for stdnoreturn.h presence

--- libopts/autoopts.h
+++ libopts/autoopts.h
@@ -32,7 +32,14 @@
 
 #ifndef AUTOGEN_AUTOOPTS_H
 #define AUTOGEN_AUTOOPTS_H
-#include <stdnoreturn.h>
+
+#ifdef HAVE_STDNORETURN_H
+# include <stdnoreturn.h>
+#else
+# ifndef noreturn
+#  define noreturn
+# endif
+#endif
 
 #define AO_NAME_LIMIT           127
 #define AO_NAME_SIZE            ((size_t)(AO_NAME_LIMIT + 1))

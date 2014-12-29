02734d8f54b3ef1d2381fbb1aa6e55c68511baaf
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-12-29 11:47:39 +0200

    send the Netmask when an IPv6 Address is assigned

--- src/worker-vpn.c
+++ src/worker-vpn.c
@@ -1555,10 +1555,15 @@ static int connect_handler(worker_st * ws)
 			SEND_ERR(ret);
 		} else {
 			ret =
-			    cstp_printf(ws, "X-CSTP-Address: %s/%u\r\n",
-				       ws->vinfo.ipv6, ws->vinfo.ipv6_prefix);
+			    cstp_printf(ws, "X-CSTP-Address: %s\r\n",
+				       ws->vinfo.ipv6);
 			SEND_ERR(ret);
 		}
+
+		ret =
+		    cstp_printf(ws, "X-CSTP-Netmask: %s/%u\r\n",
+			       ws->vinfo.ipv6, ws->vinfo.ipv6_prefix);
+		SEND_ERR(ret);
 	}
 
 	for (i = 0; i < ws->vinfo.dns_size; i++) {

a2f031a0185baaaf768509247f44de18590da839
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-02-22 14:07:15 +0100

    Allow compilation with local libopts

--- src/common.mk
+++ src/common.mk
@@ -1,7 +1,9 @@
 
 if NEED_LIBOPTS
 NEEDED_LIBOPTS = ../libopts/libopts.a
+NEEDED_LIBOPTS2 = ../../libopts/libopts.a
 else
 NEEDED_LIBOPTS = $(LIBOPTS_LDADD)
+NEEDED_LIBOPTS2 = $(LIBOPTS_LDADD)
 endif
 

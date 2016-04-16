b088d2df7363fc026594842ac8757fdb15c5761c
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2016-04-16 10:13:22 +0200

    Makefiles: combined the rules for local libraries

--- src/common.mk
+++ src/common.mk
@@ -1,9 +1,25 @@
+AM_CPPFLAGS = 
 
 if NEED_LIBOPTS
-NEEDED_LIBOPTS = ../libopts/libopts.a
-NEEDED_LIBOPTS2 = ../../libopts/libopts.a
+NEEDED_LIBOPTS = $(top_srcdir)/libopts/libopts.a
 else
 NEEDED_LIBOPTS = $(LIBOPTS_LDADD)
-NEEDED_LIBOPTS2 = $(LIBOPTS_LDADD)
 endif
 
+if LOCAL_TALLOC
+AM_CPPFLAGS += -I$(top_srcdir)/src/ccan/talloc
+endif
+
+if LOCAL_HTTP_PARSER
+AM_CPPFLAGS += -I$(top_srcdir)/src/http-parser/
+NEEDED_HTTP_PARSER_LIBS = 
+else
+NEEDED_HTTP_PARSER_LIBS = $(HTTP_PARSER_LIBS)
+endif
+
+if LOCAL_PROTOBUF_C
+AM_CPPFLAGS += -I$(top_srcdir)/src/protobuf/
+NEEDED_LIBPROTOBUF_LIBS = $(top_srcdir)/src/protobuf/libprotobuf.a
+else
+NEEDED_LIBPROTOBUF_LIBS = $(LIBPROTOBUF_C_LIBS)
+endif

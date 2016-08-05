a581d0babb22897bc77ee8057d72281177ac2c1a
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-08-05 09:04:33 +0200

    removed support for chacha20-poly1305 using the legacy protocol

--- src/worker-http.c
+++ src/worker-http.c
@@ -40,7 +40,6 @@
 
 #define CS_AES128_GCM "OC-DTLS1_2-AES128-GCM"
 #define CS_AES256_GCM "OC-DTLS1_2-AES256-GCM"
-#define CS_CHACHA20_POLY1305 "OC2-DTLS1_2-CHACHA20-POLY1305"
 
 struct known_urls_st {
 	const char *url;
@@ -122,20 +121,7 @@ static const dtls_ciphersuite_st ciphersuites[] = {
 	 .gnutls_kx = GNUTLS_KX_RSA,
 	 .gnutls_cipher = GNUTLS_CIPHER_3DES_CBC,
 	 .server_prio = 1,
-	 },
-#if GNUTLS_VERSION_NUMBER >= 0x030400
-	{
-	 .oc_name = CS_CHACHA20_POLY1305,
-	 .gnutls_name =
-	 "NONE:+VERS-DTLS1.2:+COMP-NULL:+CHACHA20-POLY1305:+AEAD:+PSK:%COMPAT:+SIGN-ALL",
-	 .gnutls_version = GNUTLS_DTLS1_2,
-	 .gnutls_mac = GNUTLS_MAC_AEAD,
-	 .gnutls_kx = GNUTLS_KX_PSK,
-	 .gnutls_cipher = GNUTLS_CIPHER_CHACHA20_POLY1305,
-	 .txt_version = "3.4.8",
-	 .server_prio = 40
-	},
-#endif
+	 }
 };
 
 #ifdef HAVE_LZ4

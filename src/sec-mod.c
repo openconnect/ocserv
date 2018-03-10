f73aa164c3f81a7d4ae6342844c30e8d8b811763
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2018-03-10 19:18:25 +0100

    tlslib: added support for gnutls 3.6.3 (unreleased)
    That adds support for GNUTLS_PRIVKEY_INFO_PK_ALGO_BITS which is
    necessary for RSA-PSS private keys.
    
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@gnutls.org>

--- src/sec-mod.c
+++ src/sec-mod.c
@@ -206,6 +206,7 @@ int process_worker_packet(void *pool, int cfd, pid_t pid, sec_mod_st * sec, cmd_
 	gnutls_datum_t data, out;
 	int ret;
 	SecOpMsg *op;
+	unsigned bits;
 #if GNUTLS_VERSION_NUMBER >= 0x030600
 	SecGetPkMsg *pkm;
 #endif
@@ -233,6 +234,8 @@ int process_worker_packet(void *pool, int cfd, pid_t pid, sec_mod_st * sec, cmd_
 		}
 
 		pkm->pk = gnutls_privkey_get_pk_algorithm(sec->key[i], NULL);
+		pkm->pk = gnutls_privkey_get_pk_algorithm(sec->key[i], &bits);
+		pkm->bits = bits;
 
 		ret = send_msg(pool, cfd, CMD_SEC_GET_PK, pkm,
 			       (pack_size_func) sec_get_pk_msg__get_packed_size,
